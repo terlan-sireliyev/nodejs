98f093c70efc3bd6f173ddfda4748ee7656de3a1
terlan-sireliyev <sireliyev95@list.com>
2024-03-07 03:16:49 -0800

    add again task

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-import { fs } from "fs";
+// import { fs } from "fs";
+
+const [, , emr, ...data] = process.argv;
+const key = "ali";
+let count = 0;
+if (emr === "add") {
+  for (let i = 0; i < data.length; i++) {
+    if (data[i] === key) {
+      count++;
+    }
+  }
+  console.log(count++);
+
+  //   console.log(`${data}`);
+}
